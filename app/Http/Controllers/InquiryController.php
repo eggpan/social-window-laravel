cadb9d5cdef5c217640a5828f998a3b77c4bb63e
Takashi Sakamoto <eggpan@eggpan.net>
2023-01-22 02:27:02 +0000

    2-1

---
+++ app/Http/Controllers/InquiryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InquiryController extends Controller
+{
+    // ここから追加
+    public function index()
+    {
+        return 'hello world!';
+    }
+    // ここまで追加
+}
