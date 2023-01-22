e03388a6d54819efdf26ae1415af5782b7c859c5
Takashi Sakamoto <eggpan@eggpan.net>
2023-01-22 03:13:37 +0000

    3-2 add

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ use App\Http\Controllers\InquiryController;
 Route::get('/', [InquiryController::class, 'index'])->name('index');
 Route::post('inquiry', [InquiryController::class, 'postInquiry'])->name('inquiry');
 Route::get('confirm', [InquiryController::class, 'showConfirm'])->name('confirm');
+Route::post('confirm', [InquiryController::class, 'postConfirm'])->name('confirm.post');
